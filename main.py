3de9171d1697d1bdd6852d0a63fc258b2ddceb4b
rafau-stdnt <148192779+rafau-stdnt@users.noreply.github.com>
2023-12-24 00:21:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import clips
 import PySimpleGUI as sg
 
+def createQuestionLayout(question, answers):
+    return [
+                [sg.Text('Typeface Expert System',justification='center', font=("Helvetica", 40))],
+                [sg.Text(key='-question-', pad=(0, 10), text=question, justification='center', font=("Helvetica", 20))],
+                [[sg.Button(key=answer, button_text=answer)] for answer in answers]
+            ]
+                
+def createTypefaceLayout(typeface):
+    return [
+                [sg.Text('Typeface Expert System',justification='center', font=("Helvetica", 40))],
+                [sg.Text(key='-question-', pad=(0, 10), text=f'Perfect font for you: {typeface}', justification='center', font=("Helvetica", 20))],
+                [sg.Button('New')],
+                [sg.Cancel()]
+            ]
 
+def createExitLayout():
+    return [
+                [sg.Text('Typeface Expert System',justification='center', font=("Helvetica", 40))],
+                [sg.Text(key='-question-', pad=(0, 10), text=f'Get out of my flowchart!', justification='center', font=("Helvetica", 20))],
+                [sg.Button('New')],
+                [sg.Cancel()]
+            ]
 
 def main():
-    sg.theme('DarkBlue')
-    layout = [[sg.Text('Typeface Expert System',justification='center', font=("Helvetica", 40))], [sg.Button("Let's choose font")]]
+    sg.theme('DarkBlue12')
+    layout = [[]]
     
-    window = sg.Window('Typeface Expert System', layout, size=(800, 800))
-
- 
+    window = sg.Window('Typeface Expert System', layout, size=(600, 400), finalize=True)
 
     # -----------------------
     env = clips.Environment()
@@ -26,37 +45,67 @@ def main():
         return
 
     while 1:
+        ifExit = None
+        typeface = None
+
+        layout = createQuestionLayout(message['question'], message['answers'])
+        window = sg.Window('Typeface Expert System', layout, size=(600, 400), finalize=True)
+
         event, values = window.read()
-        if event == "Let's choose font":
-            exit() # działa
 
-
-
-        print('Question:', message['question'])
-        answers = message['answers']
-        print("Options:", ", ".join(answers))
-
-        user_input = input("Your answer: ").strip()
-
-        print({message["name"]}, {user_input})
-
-        if user_input in answers:
-            message.modify_slots(name=message["name"], answers=[user_input])
+        if event in message['answers']:
+            message.modify_slots(name=message["name"], answers=[event])
         else:
             print("Invalid response, please try again.")
 
         env.run()
     
         for fact in env.facts():
-            if fact.template.name == 'message':
-                message = fact
-            elif fact.template.name == 'exit':
+            if fact.template.name == 'exit':
                 ifExit = fact
                 print('The end flowchart, exit without chosen typeface.')
             elif fact.template.name == 'typeface':
                 typeface = fact
-                exit()           
+                # exit()  
 
+        if typeface is not None:
+            layout = createTypefaceLayout(typeface['name'])
+            window = sg.Window('Typeface Expert System', layout, size=(600, 400), finalize=True)
+            event, values = window.read()
+
+            if event == 'New':
+                env.reset()
+                env.run()
+
+                for fact in env.facts():
+                    if fact.template.name == 'message':
+                        message = fact
+
+                continue;
+
+            if event == 'Cancel':
+                break;
+        
+        if ifExit is not None:
+            window = sg.Window('Typeface Expert System', createExitLayout(), size=(600, 400), finalize=True)
+            event, values = window.read()
+
+            if event == 'New':
+                env.reset()
+                env.run()
+
+                for fact in env.facts():
+                    if fact.template.name == 'message':
+                        message = fact
+
+                continue;
+
+            if event == 'Cancel':
+                break;
+            # break
+        # if typeface:
+        #     print(typeface)         
+    window.close()
         
 if __name__ == "__main__":
     main()
